9a5ffc99f5540124ec167c6c9b6fc94ad8c9ab2b
Stephen Chavez <stephen@dicesoft.net>
2015-10-05 20:33:23 -0600

    Finally working blendingg code

--- app/src/main/java/net/dicesoft/colorblender_android/ColorBlenderMainActivity.java
+++ app/src/main/java/net/dicesoft/colorblender_android/ColorBlenderMainActivity.java
@@ -1,6 +1,5 @@
 package net.dicesoft.colorblender_android;
 
-import android.content.ComponentName;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
@@ -9,18 +8,38 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.SeekBar;
 
-public class ColorBlenderMainActivity extends AppCompatActivity {
+public class ColorBlenderMainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     private final String MY_COLOR_PICKER_APP = "msud.cs390H.ACTION_COLOR";
-    private final int CHOOSE_COLOR = 1;
-    private int color1;
-    private Color color2;
+
+
+    private View colorView;
+    private View colorView2;
+    SeekBar colorBlenderSeekbar;
+
+    // custom intent request codes
+    private final int FIRST_COLOR = 1;
+    private final int SECOND_COLOR = 2;
+
+    private int color1 = 50;
+    private int color2 = 150;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_blender_main);
+
+        colorView = findViewById(R.id.colorView);
+
+        colorView2 = findViewById(R.id.colorView2);
+
+        colorBlenderSeekbar = (SeekBar) findViewById(R.id.seekbar_colorblender);
+        colorBlenderSeekbar.setOnSeekBarChangeListener(this);
+        colorBlenderSeekbar.setMax(255);
+        colorBlenderSeekbar.setProgress(127);
+
     }
 
     @Override
@@ -50,11 +69,13 @@ public class ColorBlenderMainActivity extends AppCompatActivity {
         // pickColorIntent.setComponent(new ComponentName("net.dicesoft", MY_COLOR_PICKER_APP));
         switch (v.getId())  {
             case R.id.btn_pickcolor1:
-                startActivityForResult(pickColorIntent, CHOOSE_COLOR);
+                startActivityForResult(pickColorIntent, FIRST_COLOR);
 
 
                 break;
             case R.id.btn_pickcolor2:
+                // pickColorIntent.putExtra("secondColor", "1");
+                startActivityForResult(pickColorIntent, SECOND_COLOR);
                 break;
             default:
                 break;
@@ -64,18 +85,77 @@ public class ColorBlenderMainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // Check which request we're responding to
-        if (requestCode == CHOOSE_COLOR) {
+        if (requestCode == FIRST_COLOR) {
             // Make sure the request was successful
             if (resultCode == RESULT_OK) {
                 Bundle extras = data.getExtras();
-                Log.d("color", String.valueOf(extras.getInt("red")));
+               // Log.d("color", String.valueOf(extras.getInt("red")));
                 color1 = Color.rgb(extras.getInt("red"), extras.getInt("green"), extras.getInt("blue"));
 
-                View colorView = findViewById(R.id.colorView);
+                colorView = findViewById(R.id.colorView);
 
                 colorView.setBackgroundColor(color1);
             }
+        } else if (requestCode == SECOND_COLOR) {
+
+            if (resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+
+                //Log.d("color", String.valueOf(extras.getInt("red")));
+                color2 = Color.rgb(extras.getInt("red"), extras.getInt("green"), extras.getInt("blue"));
+
+                colorView2 = findViewById(R.id.colorView2);
+
+                colorView2.setBackgroundColor(color2);
+            }
         }
+
+
     }
 
+    public int mixColors(int col1, int col2, int ratio) {
+        int r1, g1, b1, r2, g2, b2;
+
+        r1 = Color.red(col1);
+        g1 = Color.green(col1);
+        b1 = Color.blue(col1);
+
+        r2 = Color.red(col2);
+        g2 = Color.green(col2);
+        b2 = Color.blue(col2);
+
+        int r3 = ratio* (r1 + r2);
+        int g3 = ratio * (g1 + g2);
+        int b3 = ratio * (b1 + b2);
+
+        return Color.rgb(r3/2, g3/2, b3/2);
+    }
+
+    public int clipNumber(int number, int min,int max) {
+        return Math.max(min, Math.min(number, max));
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        int newColor = 0;
+
+        if (seekBar.getId() == R.id.seekbar_colorblender) {
+            newColor = mixColors(color1, color2, progress / 100);
+        }
+//        newColor
+      //  colorView.setBackgroundColor(newColor);
+        colorView2.setAlpha((float) progress/255);
+    }
+
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
 }
